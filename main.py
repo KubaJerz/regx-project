62187e30cc7ec969a92f610249046c70cc61d0de
KubaJerz <Koobman.kj@gmail.com>
2025-04-08 20:36:59 -0400

    done step one

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 from tree import Tree
 import argparse
-from routines import check_empty, check_has_epsilon
+from routines import check_empty, check_has_epsilon, check_has_nonepsilon, check_uses_a, create_not_using, check_is_infinite, check_starts_with, check_ends_with, create_reverse, create_prefixes, create_bs_for_a, create_insert, create_strip
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_mutually_exclusive_group(required=True)
+    # parser.add_mutually_exclusive_group(required=True)
     parser.add_argument('--no-op', action='store_true')
     parser.add_argument('--simplify', action='store_true')
     parser.add_argument('--empty', action='store_true')
     parser.add_argument('--has-epsilon', action='store_true')
-    parser.add_argument('--has-noepsilon', action='store_true')
+    parser.add_argument('--has-nonepsilon', action='store_true')
     parser.add_argument('--uses')
     parser.add_argument('--not-using')
     parser.add_argument('--infinite', action='store_true')
@@ -22,9 +22,6 @@ def parse_args():
     parser.add_argument('--insert')
     parser.add_argument('--strip')
     return parser.parse_args()
-
-
-
 
 def main():
     args = parse_args()
@@ -41,33 +38,33 @@ def main():
             if args.no_op:
                 print(postfix_tree)
             elif args.simplify:
-                pass
+                print(postfix_tree)
             elif args.empty:
                 print('yes' if check_empty(postfix_tree) else 'no')
             elif args.has_epsilon:
                 print('yes' if check_has_epsilon(postfix_tree) else 'no')
             elif args.has_nonepsilon:
-                pass
+                print('yes' if check_has_nonepsilon(postfix_tree) else 'no')
             elif args.uses:
-                pass
+                print('yes' if check_uses_a(postfix_tree, args.uses) else 'no')
             elif args.not_using:
-               pass
+               print(create_not_using(postfix_tree))
             elif args.infinite:
-                pass
+                print('yes' if check_is_infinite(postfix_tree) else 'no')
             elif args.starts_with:
-                pass
+                print('yes' if check_starts_with(postfix_tree, args.starts_with) else 'no')
             elif args.ends_with:
-                pass
+                print('yes' if check_ends_with(postfix_tree, args.ends_with) else 'no')
             elif args.reverse:
-                pass
+                print(create_reverse(postfix_tree))
             elif args.prefixes:
-                pass
+                print(create_prefixes(postfix_tree))
             elif args.bs_for_a:
-                pass
+                print(create_bs_for_a(postfix_tree))
             elif args.insert:
-                pass
+                print(create_insert(postfix_tree))
             elif args.strip:
-                pass
+                print(create_strip(postfix_tree))
         except ValueError as e:
             sys.stderr.write(f"Error: {e}")
 
